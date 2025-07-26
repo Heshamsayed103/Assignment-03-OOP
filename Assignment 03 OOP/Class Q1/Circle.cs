bb85306f94188082bcee97a8b959086145b4dd48
Heshamsayed103 <hesham58382004@gmail.com>
2025-07-26 13:11:45 +0300

    Assignment 03

---
+++ Assignment 03 OOP/Class Q1/Circle.cs
@@ -0,0 +1,26 @@
+﻿using Assignment_03_OOP.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_03_OOP.Class_Q1
+{
+    public class Circle : ICircle
+    {
+        public double Radius { get; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Area => Math.PI * Radius * Radius;
+
+        public void DisplayShapeInfo()
+        {
+            Console.WriteLine($"Shape: Circle\nRadius: {Radius}\nArea: {Area:F2}\n");
+        }
+    }
+}
---
+++ Assignment 03 OOP/Classes Q3/EmailNotificationService.cs
@@ -0,0 +1,18 @@
+﻿using Assignment_03_OOP.Interfaces_Q3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_03_OOP.Classes_Q3
+{
+    public class EmailNotificationService : INotificationService
+    {
+        public void SendNotification(string recipient, string message)
+        {
+            Console.WriteLine($"Sending Email to {recipient}: {message}");
+        }
+    }
+
+}
